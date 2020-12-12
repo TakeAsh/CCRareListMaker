8dfe475160aa0397d81919612aac4f01dcceec9a
TakeAsh <take@TakeAsh.net>
2020-12-12 19:39:41 +0900

    add: Fur Checker

---
+++ CCFurChecker.user.js
@@ -0,0 +1,59 @@
+﻿// ==UserScript==
+// @name           ConCon fur checker
+// @namespace      https://www.TakeAsh.net/
+// @ujs:published  2020-12-12 19:30
+// @version        0.1.202012121930
+// @description    check you have furs or not.
+// @author         TakeAsh68k
+// @match          https://c4.concon-collector.com/help/alllist
+// @grant          none
+// ==/UserScript==
+
+javascript:
+(function() {
+  'use strict';
+  const uriRareCCBase = '/view/default/';
+  let rares = JSON.parse(document.body.textContent || document.body.innerText)
+    .reduce(
+      (acc, cur) => {
+        let rare = new RareCC(cur);
+        if (!acc[rare.same_id]) {
+          acc[rare.same_id] = rare;
+        } else {
+          acc[rare.same_id].ids.push(rare.id);
+        }
+        return acc;
+      },
+      {}
+    );
+  let ids = Object.keys(rares)
+    .filter(id => rares[id].ids.length >= 4)
+    .sort((a, b) => (rares[b].ids.length - rares[a].ids.length) || (a.id - b.id))
+    .slice(0, 300);
+
+  let docNew = window.open('', 'FurChecker').document;
+  docNew.open('text/html');
+  docNew.write('<html>');
+  docNew.write('<head><link rel="stylesheet" type="text/css" href="/css/pc/cm.css"></head>');
+  docNew.write('<body><table border="1">');
+  docNew.write('<tr><th>#</th><th>換毛数</th><th>名前</th><th>初入手日時</th></tr>');
+  ids.forEach((id, index) => docNew.write(rares[id].toRow(index + 1)));
+  docNew.write('</table>');
+  docNew.write('</body></html>');
+  docNew.close();
+  let script = docNew.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://www.takeash.net/ConConCollector/RareCC/checkRareCC.js';
+  docNew.head.appendChild(script);
+
+  function RareCC(src) {
+    for (var p in src) {
+      this[p] = src[p];
+    }
+    this.ids = [];
+    this.toRow = (index) => '<tr><td>' + index + '</td><td>' + this.ids.length +
+      '</td><td><a href="' + uriRareCCBase + this.id + '" target="_blank">' +
+      this.title + this.name + '</a></td><td><span id="get_' + this.id +
+      '"></span></td></tr>';
+  }
+})();
---
+++ html/checkRareCC.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+(function() {
+  const uriRareBase = 'https://c4.concon-collector.com/view/default/';
+  getNodesByXpath('//span[starts-with(@id,"get_")]').forEach(node => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uriRareBase + node.id.replace(/^get_/, ''));
+    xhr.onload = function(e) {
+      let m = xhr.responseText.match(/<span\sclass="keyword">初入手日時<\/span>\s*([^<]+)</);
+      node.innerHTML = !m ? '-' : m[1];
+    };
+    xhr.send();
+  });
+
+  function getNodesByXpath(xpath, context) {
+    let itr = document.evaluate(
+      xpath,
+      context || document,
+      null,
+      XPathResult.ORDERED_NODE_ITERATOR_TYPE,
+      null
+    );
+    let nodes = [];
+    let node = null;
+    while (node = itr.iterateNext()) {
+      nodes.push(node);
+    }
+    return nodes;
+  }
+})();
