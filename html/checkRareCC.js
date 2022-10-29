066c0f3733c8781adde4d59ad43c74e0e9e5f993
TakeAsh <take@TakeAsh.net>
2022-10-29 13:33:25 +0900

    fix: failed by 'max_user_connections'

--- html/checkRareCC.js
+++ html/checkRareCC.js
@@ -1,25 +1,32 @@
 ﻿'use strict';
-(function() {
+(async function() {
   const uriRareBase = 'https://c4.concon-collector.com/view/default/';
-  getNodesByXpath('//span[starts-with(@id,"get_")]').forEach(node => {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', uriRareBase + node.id.replace(/^get_/, ''));
-    xhr.onload = function(e) {
-      let m = xhr.responseText.match(/<span\sclass="keyword">初入手日時<\/span>\s*([^<]+)</);
-      node.innerHTML = !m ? '-' : m[1];
-    };
-    xhr.send();
-  });
+  const regGotDate = /<span\sclass="keyword">初入手日時<\/span>\s*([^<]+)s*</;
+  const regTitle = /<title>([^<]+)<\/title>/;
+  const nodes = getNodesByXpath('//span[starts-with(@id,"get_")]');
+  for (const node of nodes) {
+    const response = await fetch(uriRareBase + node.id.replace(/^get_/, ''));
+    if (response.ok) {
+      const text = await response.text();
+      let m;
+      node.innerHTML =
+        (m = text.match(regGotDate)) ? m[1]
+          : (m = text.match(regTitle)) && m[1] == 'コンコンコレクター 図鑑' ? '-'
+            : text;
+    } else {
+      node.innerHTML = response.statusText;
+    }
+  }
 
   function getNodesByXpath(xpath, context) {
-    let itr = document.evaluate(
+    const itr = document.evaluate(
       xpath,
       context || document,
       null,
       XPathResult.ORDERED_NODE_ITERATOR_TYPE,
       null
     );
-    let nodes = [];
+    const nodes = [];
     let node = null;
     while (node = itr.iterateNext()) {
       nodes.push(node);
