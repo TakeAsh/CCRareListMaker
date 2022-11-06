f16fecccc055c37e84a2b0d21c7b24994a0f9400
TakeAsh <take@TakeAsh.net>
2022-11-06 14:06:25 +0900

    CCDupedChecker.user.js: fasten by muti-threading

--- CCDupedChecker.user.js
+++ CCDupedChecker.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         ConCon Duplicated Checker
 // @namespace    https://www.TakeAsh.net/
-// @version      0.1.202210291730
+// @version      0.1.202211061300
 // @description  scan concon list and order by duplication
 // @author       TakeAsh68k
 // @match        https://c4.concon-collector.com/help/alllist
@@ -17,25 +17,75 @@ javascript:
     return;
   }
   const d = document;
-  const divResult = d.createElement('div');
-  d.body.appendChild(divResult);
-  divResult.style.position = 'fixed';
-  divResult.style.bottom = '0em';
-  divResult.style.right = '0em';
-  const getDupes = async (concon) => {
-    const res = await fetch(`/view/default/${concon.id}`);
-    const text = await res.text();
-    const doc = await new DOMParser().parseFromString(text, 'text/html');
-    const anchor = doc.querySelector(`a[href="https://c4.concon-collector.com/cccontainer/pickid/${concon.id}"]`);
-    if (anchor) {
-      const m = /\（(\d+)(\+(\d+))?[^\)]+\）/.exec(anchor.textContent);
-      const same = m[1] * 1;
-      const others = (m[3] || 0) * 1;
-      concon.dupes = same + others;
-      concon.dupesDetail = `${same}+${others}`;
+  const maxThreads = 24;
+  const idDivResult = 'DupedCheckerResult';
+  const start = Date.now();
+  const divResult = d.getElementById(idDivResult) || d.createElement('div');
+  if (!divResult.id) {
+    d.body.appendChild(divResult);
+    divResult.id = idDivResult;
+    divResult.style.position = 'fixed';
+    divResult.style.bottom = '0em';
+    divResult.style.right = '0em';
+  }
+  const range = (start, stop, step) =>
+    Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step));
+  const srcWorker = `
+  self.addEventListener(
+    'message',
+    async (event) => {
+      const concons = event.data;
+      for (const concon of concons) {
+        const res = await fetch(\`\${location.origin}/view/default/\${concon.id}\`);
+        const text = await res.text();
+        const anchor = /<a\\shref="[^"]+\\/cccontainer\\/pickid\\/[^"]+">([^<]+)<\\/a>/.exec(text);
+        if (anchor) {
+          const m = /\\（(\\d+)(\\+(\\d+))?[^\\)]+\\）/.exec(anchor[1]);
+          const same = m[1] * 1;
+          const others = (m[3] || 0) * 1;
+          concon.dupes = same + others;
+          concon.dupesDetail = \`\${same}+\${others}\`;
+        }
+      }
+      postMessage(concons);
     }
-    return concon;
+  );`;
+  const urlWorker = URL.createObjectURL(new Blob([srcWorker], { type: 'application/javascript' }));
+  const createWorker = (concons) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(urlWorker);
+      worker.addEventListener('message', event => resolve(event.data));
+      worker.addEventListener('error', reject);
+      worker.postMessage(concons);
+    });
   };
+  const res = await fetch('/help/alllist');
+  const list = await res.json();
+  const baseConCons = list.filter(concon => concon.id == concon.same_id);
+  console.log(baseConCons);
+  let current = 0;
+  const updateStatus = (concons) => {
+    if (concons) { current += concons.length; }
+    divResult.textContent = `${current}/${baseConCons.length}`;
+    return concons;
+  };
+  updateStatus();
+  const lenUnit = baseConCons.length / (maxThreads * (maxThreads + 1) / 2.0);
+  const from = (i) => Math.ceil(lenUnit * i * (i + 1) / 2.0);
+  const dividedConCons = range(0, maxThreads - 1, 1)
+    .map((i) => baseConCons.slice(from(i), from(i + 1)));
+  console.log(dividedConCons);
+  const scannedConCons = await Promise.all(
+    dividedConCons.map((concons) => createWorker(concons).then(updateStatus)));
+  URL.revokeObjectURL(urlWorker);
+  console.log(`scannedConCons: ${current}`);
+  console.log(scannedConCons);
+  divResult.textContent = '';
+  const dupedConCons = scannedConCons.reduce(
+    (acc, cur) => acc.concat(cur.filter(concon => (concon.dupes || 0) > 0)),
+    []
+  ).sort((a, b) => b.dupes - a.dupes || a.id - b.id)
+    .slice(0, 20);
   const toTable = (concons) => {
     const table = d.createElement('table');
     table.style.backgroundColor = 'rgba(64, 64, 64, 0.6)';
@@ -66,18 +116,6 @@ javascript:
     });
     return table;
   };
-  const res = await fetch('/help/alllist');
-  const list = await res.json();
-  const concons = list.filter(concon => concon.id == concon.same_id);
-  for (let i = 0; i < concons.length; ++i) {
-    if (i % 20 == 0) {
-      divResult.textContent = `${i + 1}/${concons.length}`;
-    }
-    concons[i] = await getDupes(concons[i]);
-  }
-  divResult.textContent = '';
-  const dupedConCons = concons.filter(concon => (concon.dupes || 0) > 0)
-    .sort((a, b) => b.dupes - a.dupes || a.id - b.id)
-    .slice(0, 20);
   divResult.appendChild(toTable(dupedConCons));
+  console.log(`${(Date.now() - start) / 1000} sec`);
 })();
