51463bbb93ef1abc31d5bed57728103f9bfbe52f
TakeAsh <take@TakeAsh.net>
2022-11-19 08:38:49 +0900

    import WorkerManager class from file

--- CCDupedChecker.user.js
+++ CCDupedChecker.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         ConCon Duplicated Checker
 // @namespace    https://www.TakeAsh.net/
-// @version      0.1.202211190400
+// @version      0.1.202211190500
 // @description  scan concon list and order by duplication
 // @author       TakeAsh68k
 // @match        https://c4.concon-collector.com/help/alllist
@@ -18,241 +18,156 @@ javascript:
   }
   const d = document;
   const maxThreads = 24;
-  const start = Date.now();
-  const range = (start, stop, step) =>
-    Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step));
-  class StatusManager {
-    static #div;
-    static #aborted = false;
-    static #current = 0;
-    static #max = 0;
-    static set id(id) {
-      this.#div = d.getElementById(id) || d.createElement('div');
-      if (!this.#div.id) {
-        d.body.appendChild(this.#div);
-        this.#div.id = id;
-        this.#div.style.position = 'fixed';
-        this.#div.style.bottom = '0em';
-        this.#div.style.right = '0em';
+  const script = d.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://www.takeash.net/ConConCollector/RareCC/modules/WorkerManager.js';
+  script.addEventListener(
+    'load',
+    async () => {
+      const start = Date.now();
+      class StatusManager {
+        static #div;
+        static #aborted = false;
+        static #current = 0;
+        static #max = 0;
+        static set id(id) {
+          this.#div = d.getElementById(id) || d.createElement('div');
+          if (!this.#div.id) {
+            d.body.appendChild(this.#div);
+            this.#div.id = id;
+            this.#div.style.position = 'fixed';
+            this.#div.style.bottom = '0em';
+            this.#div.style.right = '0em';
+          }
+        }
+        static set max(max) {
+          this.#max = max;
+        }
+        static get current() {
+          return this.#current;
+        }
+        static reportProgress(progress) {
+          const self = StatusManager;
+          if (self.#aborted) { return; }
+          if (progress) {
+            /* console.log(progress); */
+            ++self.#current;
+          }
+          if (self.#current % 50 != 0) { return; }
+          self.#div.textContent = `${self.#current}/${self.#max}`;
+        }
+        static abort(message) {
+          this.#aborted = true;
+          this.#div.textContent = message;
+        }
+        static toTable(concons) {
+          this.#div.textContent = '';
+          const table = d.createElement('table');
+          table.style.backgroundColor = 'rgba(64, 64, 64, 0.6)';
+          const thead = d.createElement('thead');
+          const tbody = d.createElement('tbody');
+          [thead, tbody].forEach(section => table.appendChild(section));
+          ['Dupes', 'Rarity', 'Name'].forEach(label => {
+            const th = d.createElement('th');
+            thead.appendChild(th);
+            th.textContent = label;
+          });
+          concons.forEach(concon => {
+            const tr = d.createElement('tr');
+            tbody.appendChild(tr);
+            const tdDupes = d.createElement('td');
+            const tdRarity = d.createElement('td');
+            const tdName = d.createElement('td');
+            [tdDupes, tdRarity, tdName].forEach(td => tr.appendChild(td));
+            tdDupes.textContent = `${concon.dupes} (${concon.dupesDetail})`;
+            tdDupes.style.textAlign = 'center';
+            tdRarity.textContent = concon.rarity;
+            tdRarity.style.textAlign = 'center';
+            const aName = d.createElement('a');
+            tdName.appendChild(aName);
+            aName.href = `/view/default/${concon.id}`;
+            aName.target = '_blank';
+            aName.textContent = [concon.title, concon.name].join(' ');
+          });
+          this.#div.appendChild(table);
+        }
       }
-    }
-    static set max(max) {
-      this.#max = max;
-    }
-    static get current() {
-      return this.#current;
-    }
-    static reportProgress(progress) {
-      const self = StatusManager;
-      if (self.#aborted) { return; }
-      if (progress) {
-        /* console.log(progress); */
-        ++self.#current;
-      }
-      if (self.#current % 50 != 0) { return; }
-      self.#div.textContent = `${self.#current}/${self.#max}`;
-    }
-    static abort(message) {
-      this.#aborted = true;
-      this.#div.textContent = message;
-    }
-    static toTable(concons) {
-      this.#div.textContent = '';
-      const table = d.createElement('table');
-      table.style.backgroundColor = 'rgba(64, 64, 64, 0.6)';
-      const thead = d.createElement('thead');
-      const tbody = d.createElement('tbody');
-      [thead, tbody].forEach(section => table.appendChild(section));
-      ['Dupes', 'Rarity', 'Name'].forEach(label => {
-        const th = d.createElement('th');
-        thead.appendChild(th);
-        th.textContent = label;
-      });
-      concons.forEach(concon => {
-        const tr = d.createElement('tr');
-        tbody.appendChild(tr);
-        const tdDupes = d.createElement('td');
-        const tdRarity = d.createElement('td');
-        const tdName = d.createElement('td');
-        [tdDupes, tdRarity, tdName].forEach(td => tr.appendChild(td));
-        tdDupes.textContent = `${concon.dupes} (${concon.dupesDetail})`;
-        tdDupes.style.textAlign = 'center';
-        tdRarity.textContent = concon.rarity;
-        tdRarity.style.textAlign = 'center';
-        const aName = d.createElement('a');
-        tdName.appendChild(aName);
-        aName.href = `/view/default/${concon.id}`;
-        aName.target = '_blank';
-        aName.textContent = [concon.title, concon.name].join(' ');
-      });
-      this.#div.appendChild(table);
-    }
-  }
-  class WorkerManager {
-    #url = null;
-    #reportProgress = (progress) => { };
-    #workers = [];
-    #range = (max) => Array.from({ length: max }, (_, i) => (i));
-    /**
-     * function as worker source
-     * @param {() => void} fnc
-     * @memberof WorkerManager
-     */
-    set source(fnc) {
-      if (typeof fnc != 'function') { return; }
-      this.#url = URL.createObjectURL(
-        new Blob([`(${fnc})();`], { type: 'application/javascript' }));
-    }
-    /**
-     * function called by the worker while running.
-     * @memberof WorkerManager
-     */
-    get reportProgress() {
-      return this.#reportProgress;
-    }
-    set reportProgress(fnc) {
-      if (typeof fnc != 'function') { return; }
-      this.#reportProgress = fnc;
-    }
-    /**
-     * release resources
-     * @memberof WorkerManager
-     */
-    dispose() {
-      URL.revokeObjectURL(this.#url);
-      this.#url = null;
-      this.#reportProgress = null;
-      this.#workers = null;
-    }
-    /**
-     * create a Promise including Worker and start it
-     * @param {*} id Worker id
-     * @param {*} initialData Initial data for worker
-     * @return {Promise} Promise including Worker
-     * @memberof WorkerManager
-     */
-    create(id, initialData) {
-      return new Promise((resolve, reject) => {
-        const worker = new Worker(this.#url);
-        worker.reportProgress = this.#reportProgress;
-        worker.addEventListener(
+      const wm = new WorkerManager();
+      wm.source = () => {
+        const regTitle = /<title>([^<]+)<\/title>/;
+        const regAnchor = /<a\shref="[^"]+\/cccontainer\/pickid\/[^"]+">([^<]+)<\/a>/;
+        const regDupes = /\（(\d+)(\+(\d+))?[^\)]+\）/;
+        let isCancelled = false;
+        self.addEventListener(
           'message',
-          (event) => {
-            const data = event.data;
-            switch (data.type) {
-              case 'Progress':
-                event.target.reportProgress(data.progress);
-                break;
-              case 'Completed':
-                console.log(data.message);
-                resolve(data.result);
-                break;
-              case 'Error':
-                console.log(data.message);
-                reject(data.message);
-                break;
+          async (event) => {
+            if (event.data == 'cancel') {
+              isCancelled = true;
+              return;
             }
+            const id = event.data.id;
+            const concons = event.data.data;
+            let index = 0;
+            for (const concon of concons) {
+              if (isCancelled) {
+                postMessage({ type: 'Error', message: `worker[${id}]: cancelled`, });
+                return;
+              }
+              const res = await fetch(`${location.origin}/view/default/${concon.id}`);
+              const text = await res.text();
+              let m;
+              if (!(m = text.match(regTitle)) || m[1] != 'コンコンコレクター 図鑑') {
+                postMessage({ type: 'Error', message: `worker[${id}]: ${text}`, });
+                return;
+              }
+              const anchor = regAnchor.exec(text);
+              if (anchor) {
+                const m = regDupes.exec(anchor[1]);
+                const same = m[1] * 1;
+                const others = (m[3] || 0) * 1;
+                concon.dupes = same + others;
+                concon.dupesDetail = `${same}+${others}`;
+              }
+              postMessage({
+                type: 'Progress',
+                progress: {
+                  id: id,
+                  message: `${++index}/${concons.length}`,
+                },
+              });
+            }
+            postMessage({
+              type: 'Completed',
+              message: `worker[${id}]: completed`,
+              result: concons,
+            });
           }
         );
-        worker.addEventListener('error', reject);
-        worker.postMessage({ id: id, data: initialData });
-        this.#workers.push(worker);
-      });
-    }
-    /**
-     * create workers from data and run them
-     * @param {Array} data array of initial data
-     * @param {number} threads
-     * @return {Promise} worker results
-     * @memberof WorkerManager
-     */
-    async run(data, threads) {
-      const from = (i) => Math.ceil(data.length * i / threads);
-      const divided = this.#range(threads).map((i) => data.slice(from(i), from(i + 1)));
-      console.log(divided);
-      return await Promise.all(divided.map((part, i) => this.create(i, part)));
-    }
-    /**
-     * notify all workers that they should be cancelled
-     * @memberof WorkerManager
-     */
-    cancelAll() {
-      this.#workers.forEach((worker) => { worker.postMessage('cancel'); })
-    }
-  }
-  const wm = new WorkerManager();
-  wm.source = () => {
-    const regTitle = /<title>([^<]+)<\/title>/;
-    const regAnchor = /<a\shref="[^"]+\/cccontainer\/pickid\/[^"]+">([^<]+)<\/a>/;
-    const regDupes = /\（(\d+)(\+(\d+))?[^\)]+\）/;
-    let isCancelled = false;
-    self.addEventListener(
-      'message',
-      async (event) => {
-        if (event.data == 'cancel') {
-          isCancelled = true;
-          return;
-        }
-        const id = event.data.id;
-        const concons = event.data.data;
-        let index = 0;
-        for (const concon of concons) {
-          if (isCancelled) {
-            postMessage({ type: 'Error', message: `worker[${id}]: cancelled`, });
-            return;
-          }
-          const res = await fetch(`${location.origin}/view/default/${concon.id}`);
-          const text = await res.text();
-          let m;
-          if (!(m = text.match(regTitle)) || m[1] != 'コンコンコレクター 図鑑') {
-            postMessage({ type: 'Error', message: `worker[${id}]: ${text}`, });
-            return;
-          }
-          const anchor = regAnchor.exec(text);
-          if (anchor) {
-            const m = regDupes.exec(anchor[1]);
-            const same = m[1] * 1;
-            const others = (m[3] || 0) * 1;
-            concon.dupes = same + others;
-            concon.dupesDetail = `${same}+${others}`;
-          }
-          postMessage({
-            type: 'Progress',
-            progress: {
-              id: id,
-              message: `${++index}/${concons.length}`,
-            },
-          });
-        }
-        postMessage({
-          type: 'Completed',
-          message: `worker[${id}]: completed`,
-          result: concons,
+      };
+      wm.reportProgress = StatusManager.reportProgress;
+      const res = await fetch('/help/alllist');
+      const list = await res.json();
+      const baseConCons = list.filter(concon => concon.id == concon.same_id);
+      console.log(baseConCons);
+      StatusManager.id = 'DupedCheckerResult';
+      StatusManager.max = baseConCons.length;
+      StatusManager.reportProgress();
+      const scannedConCons = await wm.run(baseConCons, maxThreads)
+        .catch((err) => {
+          StatusManager.abort(err);
+          wm.cancelAll();
         });
-      }
-    );
-  };
-  wm.reportProgress = StatusManager.reportProgress;
-  const res = await fetch('/help/alllist');
-  const list = await res.json();
-  const baseConCons = list.filter(concon => concon.id == concon.same_id);
-  console.log(baseConCons);
-  StatusManager.id = 'DupedCheckerResult';
-  StatusManager.max = baseConCons.length;
-  StatusManager.reportProgress();
-  const scannedConCons = await wm.run(baseConCons, maxThreads)
-    .catch((err) => {
-      StatusManager.abort(err);
-      wm.cancelAll();
-    });
-  wm.dispose();
-  console.log(`scannedConCons: ${StatusManager.current}`);
-  if (!scannedConCons) { return; }
-  console.log(scannedConCons);
-  const dupedConCons = scannedConCons
-    .reduce((acc, cur) => acc.concat(cur.filter(cc => (cc.dupes || 0) > 0)), [])
-    .sort((a, b) => b.dupes - a.dupes || a.id - b.id)
-    .slice(0, 20);
-  StatusManager.toTable(dupedConCons);
-  console.log(`${(Date.now() - start) / 1000} sec`);
+      wm.dispose();
+      console.log(`scannedConCons: ${StatusManager.current}`);
+      if (!scannedConCons) { return; }
+      console.log(scannedConCons);
+      const dupedConCons = scannedConCons
+        .reduce((acc, cur) => acc.concat(cur.filter(cc => (cc.dupes || 0) > 0)), [])
+        .sort((a, b) => b.dupes - a.dupes || a.id - b.id)
+        .slice(0, 20);
+      StatusManager.toTable(dupedConCons);
+      console.log(`${(Date.now() - start) / 1000} sec`);
+    }
+  );
+  d.head.appendChild(script);
 })();
--- html/checkRareCC.js
+++ html/checkRareCC.js
@@ -2,175 +2,92 @@
 (async () => {
   const d = document;
   const maxThreads = 24;
-  class WorkerManager {
-    #url = null;
-    #reportProgress = (progress) => { };
-    #workers = [];
-    #range = (max) => Array.from({ length: max }, (_, i) => (i));
-    /**
-     * function as worker source
-     * @param {() => void} fnc
-     * @memberof WorkerManager
-     */
-    set source(fnc) {
-      if (typeof fnc != 'function') { return; }
-      this.#url = URL.createObjectURL(
-        new Blob([`(${fnc})();`], { type: 'application/javascript' }));
-    }
-    /**
-     * function called by the worker while running.
-     * @memberof WorkerManager
-     */
-    get reportProgress() {
-      return this.#reportProgress;
-    }
-    set reportProgress(fnc) {
-      if (typeof fnc != 'function') { return; }
-      this.#reportProgress = fnc;
-    }
-    /**
-     * release resources
-     * @memberof WorkerManager
-     */
-    dispose() {
-      URL.revokeObjectURL(this.#url);
-      this.#url = null;
-      this.#reportProgress = null;
-      this.#workers = null;
-    }
-    /**
-     * create a Promise including Worker and start it
-     * @param {*} id Worker id
-     * @param {*} initialData Initial data for worker
-     * @return {Promise} Promise including Worker
-     * @memberof WorkerManager
-     */
-    create(id, initialData) {
-      return new Promise((resolve, reject) => {
-        const worker = new Worker(this.#url);
-        worker.reportProgress = this.#reportProgress;
-        worker.addEventListener(
+  const script = d.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://www.takeash.net/ConConCollector/RareCC/modules/WorkerManager.js';
+  script.addEventListener(
+    'load',
+    async () => {
+      const wm = new WorkerManager();
+      wm.source = () => {
+        const uriRareBase = 'https://c4.concon-collector.com/view/default/';
+        const regGotDate = /<span\sclass="keyword">初入手日時<\/span>\s*([^<]+)s*</;
+        const regTitle = /<title>([^<]+)<\/title>/;
+        let isCancelled = false;
+        self.addEventListener(
           'message',
-          (event) => {
-            const data = event.data;
-            switch (data.type) {
-              case 'Progress':
-                event.target.reportProgress(data.progress);
-                break;
-              case 'Completed':
-                console.log(data.message);
-                resolve(data.result);
-                break;
-              case 'Error':
-                console.log(data.message);
-                reject(data.message);
-                break;
-            }
-          }
-        );
-        worker.addEventListener('error', reject);
-        worker.postMessage({ id: id, data: initialData });
-        this.#workers.push(worker);
-      });
-    }
-    /**
-     * create workers from data and run them
-     * @param {Array} data array of initial data
-     * @param {number} threads
-     * @return {Promise} worker results
-     * @memberof WorkerManager
-     */
-    async run(data, threads) {
-      const from = (i) => Math.ceil(data.length * i / threads);
-      const divided = this.#range(threads).map((i) => data.slice(from(i), from(i + 1)));
-      console.log(divided);
-      return await Promise.all(divided.map((part, i) => this.create(i, part)));
-    }
-    /**
-     * notify all workers that they should be cancelled
-     * @memberof WorkerManager
-     */
-    cancelAll() {
-      this.#workers.forEach((worker) => { worker.postMessage('cancel'); })
-    }
-  }
-  const wm = new WorkerManager();
-  wm.source = () => {
-    const uriRareBase = 'https://c4.concon-collector.com/view/default/';
-    const regGotDate = /<span\sclass="keyword">初入手日時<\/span>\s*([^<]+)s*</;
-    const regTitle = /<title>([^<]+)<\/title>/;
-    let isCancelled = false;
-    self.addEventListener(
-      'message',
-      async (event) => {
-        if (event.data == 'cancel') {
-          isCancelled = true;
-          return;
-        }
-        const id = event.data.id;
-        const ccIds = event.data.data;
-        let result = '';
-        let index = 0;
-        for (const ccId of ccIds) {
-          if (isCancelled) {
-            postMessage({ type: 'Error', message: `worker[${id}]: cancelled`, });
-            return;
-          }
-          const response = await fetch(uriRareBase + ccId);
-          if (response.ok) {
-            const text = await response.text();
-            let m;
-            if (!(m = text.match(regTitle)) || m[1] != 'コンコンコレクター 図鑑') {
-              postMessage({ type: 'Error', message: `worker[${id}]: ${text}`, });
+          async (event) => {
+            if (event.data == 'cancel') {
+              isCancelled = true;
               return;
             }
-            result = (m = text.match(regGotDate))
-              ? m[1]
-              : '-';
-          } else {
-            result = response.statusText;
+            const id = event.data.id;
+            const ccIds = event.data.data;
+            let result = '';
+            let index = 0;
+            for (const ccId of ccIds) {
+              if (isCancelled) {
+                postMessage({ type: 'Error', message: `worker[${id}]: cancelled`, });
+                return;
+              }
+              const response = await fetch(uriRareBase + ccId);
+              if (response.ok) {
+                const text = await response.text();
+                let m;
+                if (!(m = text.match(regTitle)) || m[1] != 'コンコンコレクター 図鑑') {
+                  postMessage({ type: 'Error', message: `worker[${id}]: ${text}`, });
+                  return;
+                }
+                result = (m = text.match(regGotDate))
+                  ? m[1]
+                  : '-';
+              } else {
+                result = response.statusText;
+              }
+              postMessage({
+                type: 'Progress',
+                progress: {
+                  index: index++,
+                  ccId: ccId,
+                  result: result,
+                },
+              });
+            }
+            postMessage({
+              type: 'Completed',
+              message: `worker[${id}]: completed`,
+              result: null,
+            });
           }
-          postMessage({
-            type: 'Progress',
-            progress: {
-              index: index++,
-              ccId: ccId,
-              result: result,
-            },
-          });
-        }
-        postMessage({
-          type: 'Completed',
-          message: `worker[${id}]: completed`,
-          result: null,
+        );
+      };
+      const toCCId = (node) => node.id.replace(/^get_/, '');
+      const baseNodes = getNodesByXpath('//span[starts-with(@id,"get_")]');
+      const nodes = baseNodes.reduce(
+        (acc, cur) => {
+          acc[toCCId(cur)] = cur;
+          return acc;
+        },
+        {}
+      );
+      let count = 0;
+      let isAborted = false;
+      wm.reportProgress = (progress) => {
+        if (isAborted) { return; }
+        nodes[progress.ccId].textContent = progress.result;
+        showResult(`${++count}/${baseNodes.length}`);
+      };
+      await wm.run(baseNodes.map(node => toCCId(node)), maxThreads)
+        .then(() => { showResult('Completed', 'keyword'); })
+        .catch((err) => {
+          isAborted = true;
+          wm.cancelAll();
+          showResult(err, 'error_message');
         });
-      }
-    );
-  };
-  const toCCId = (node) => node.id.replace(/^get_/, '');
-  const baseNodes = getNodesByXpath('//span[starts-with(@id,"get_")]');
-  const nodes = baseNodes.reduce(
-    (acc, cur) => {
-      acc[toCCId(cur)] = cur;
-      return acc;
-    },
-    {}
+      wm.dispose();
+    }
   );
-  let count = 0;
-  let isAborted = false;
-  wm.reportProgress = (progress) => {
-    if (isAborted) { return; }
-    nodes[progress.ccId].textContent = progress.result;
-    showResult(`${++count}/${baseNodes.length}`);
-  };
-  await wm.run(baseNodes.map(node => toCCId(node)), maxThreads)
-    .then(() => { showResult('Completed', 'keyword'); })
-    .catch((err) => {
-      isAborted = true;
-      wm.cancelAll();
-      showResult(err, 'error_message');
-    });
-  wm.dispose();
+  d.head.appendChild(script);
 
   function getNodesByXpath(xpath, context) {
     const itr = d.evaluate(
