4313f10bf5f351da16da586573ce29fa79001bec
TakeAsh <take@TakeAsh.net>
2023-01-07 07:07:10 +0900

    add CCBossWatcher

---
+++ CCBossWatcher.user.js
@@ -0,0 +1,22 @@
+﻿// ==UserScript==
+// @name         ConCon Boss Watcher
+// @namespace    https://www.TakeAsh.net/
+// @version      0.0.2023010705
+// @description  Watch boss and beat it repeatedly.
+// @author       TakeAsh68k
+// @match        https://c4.concon-collector.com/status
+// @match        https://c4.concon-collector.com/relief/default/1
+// @match        https://c4.concon-collector.com/chat
+// @match        https://c4.concon-collector.com/chat/*
+// @match        https://c4.concon-collector.com/explore/coop/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=concon-collector.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  const d = document;
+  const script = d.createElement('script');
+  script.type = 'module';
+  script.src = 'https://www.takeash.net/ConConCollector/RareCC/watchBoss.js';
+  d.head.appendChild(script);
+})();
---
+++ html/watchBoss.js
@@ -0,0 +1,89 @@
+﻿'use strict';
+import { getNodesByXpath } from 'https://www.takeash.net/js/modules/Util.mjs';
+
+const d = document;
+const config = {
+  checkInterval: 3,
+  attackInterval: 20,
+  destoryLimit: 10,
+};
+const cookieName = '_CCCP_MyUserId';
+const myUserId = getCookie(cookieName);
+const regMyProfile = new RegExp(`\/profile\/default\/${myUserId}\\b`);
+const page = location.pathname.match(/^\/([^\/]+)/)[1];
+switch (page) {
+  case 'status':
+    status();
+    break;
+  case 'relief':
+  case 'chat':
+    relief();
+    break;
+  case 'explore':
+    explore();
+    break;
+}
+
+function status() {
+  const aProfile = getNodesByXpath('//a[text()="プロフィール"]')[0];
+  if (!aProfile) { return; }
+  const m = aProfile.href.match(/\/profile\/default\/(\d+)/);
+  if (!m || !m[1]) { return; }
+  setCookie(cookieName, m[1], 30);
+}
+
+function relief() {
+  getNodesByXpath('//a[contains(@href,"/explore/coop/")]')
+    .forEach((a) => {
+      const bossId = a.href.match(/\/explore\/coop\/(\d+)/)[1];
+      a.target = `boss${bossId}`;
+      a.click();
+    });
+  const now = new Date();
+  const hour = now.getHours();
+  const min = now.getMinutes();
+  const interval = hour == 4 && 0 <= min && min <= 31
+    ? (32 - min) * 60 * 1000
+    : config.checkInterval * 60 * 1000;
+  setInterval(() => { location = location.href; }, interval);
+}
+
+function explore() {
+  checkOk: {
+    const currentAp = getNodesByXpath('//span[text()="現在AP"]')[0];
+    if (!currentAp || currentAp.nextSibling.textContent.trim() < 3) { break checkOk; }
+    const spanRestTime = getNodesByXpath('//span[@class="keyword" and text()="残り時間"]')[0];
+    if (!spanRestTime) { break checkOk; }
+    const m = spanRestTime.nextSibling.textContent.match(/\s*((?<min>\d+)分)?(?<sec>\d+)秒/);
+    const buttonAttacks = getNodesByXpath('//input[@type="submit" and contains(@value,"攻撃する")]');
+    if (!buttonAttacks.length) { break checkOk; }
+    let buttonAttack = buttonAttacks[1] || buttonAttacks[0];
+    if (m.groups.min > config.destoryLimit) {
+      const aPlayers = getNodesByXpath('//a[contains(@href,"profile")]');
+      aPlayers.shift(); // drop finding player
+      const lastMyAttack = aPlayers.find((a) => a.href.match(regMyProfile));
+      if (lastMyAttack) {
+        const m = lastMyAttack.parentNode.textContent.match(/ダメージを与えた！\((\d+).*\)/);
+        if (m && m[1] <= config.attackInterval) { break checkOk; }
+      }
+      buttonAttack = buttonAttacks[0];
+    }
+    buttonAttack.click();
+    return;
+  }
+  setTimeout(() => { window.close(); }, 10 * 1000);
+}
+
+function getCookie(key) {
+  const m = d.cookie.match(new RegExp(key + '\\s*=\\s*([^;]+)'));
+  return m && m[1] || '';
+}
+
+function setCookie(key, value, expireDate) {
+  let cookieStr = key + '=' + value + ';path=/';
+  if (expireDate > 0) {
+    expireDate = new Date(new Date().getTime() + 60 * 60 * 24 * expireDate * 1000).toGMTString();
+    cookieStr += ';expires=' + expireDate;
+  }
+  d.cookie = cookieStr;
+}
