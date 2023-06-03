28416ee72bd470a5f0c7f1cb4bfb319721ff5fc3
TakeAsh <take@TakeAsh.net>
2023-06-03 19:41:30 +0900

    watchBoss: open boss in iframe instead of new tab - introduce prepareElement

--- html/watchBoss.js
+++ html/watchBoss.js
@@ -1,6 +1,8 @@
 ﻿'use strict';
 import { getNodesByXpath } from 'https://www.takeash.net/js/modules/Util.mjs';
+import { prepareElement, addStyle } from 'https://www.takeash.net/js/modules/PrepareElement.mjs';
 
+const w = window;
 const d = document;
 const config = {
   checkInterval: 3,
@@ -10,6 +12,7 @@ const config = {
 const cookieMyUserId = '_CCCP_MyUserId';
 const cookieKeepLogin = '_CCCP_KeepLogin';
 const cookieCredential = '_CCCP_Credential';
+const keySender = 'CCBossWatcher';
 const myUserId = getCookie(cookieMyUserId);
 const regMyProfile = new RegExp(`\/profile\/default\/${myUserId}\\b`);
 const page = location.pathname.match(/^\/([^\/]+)/)[1];
@@ -56,9 +59,9 @@ function login() {
   } else {
     const aStatus = getNodesByXpath('//a[contains(@href,"/status")]')[0];
     if (aStatus) {
-      window.open('/relief/default/1', 'reliefPage');
-      window.open('/chat', 'chatPage');
-      window.open('/rid/list', 'ridPage');
+      w.open('/relief/default/1', 'reliefPage');
+      w.open('/chat', 'chatPage');
+      w.open('/rid/list', 'ridPage');
       aStatus.click();
     }
   }
@@ -84,38 +87,55 @@ function status() {
   }
   const aAccount = getNodesByXpath('//a[contains(@href,"/account")]')[0];
   if (aAccount) {
-    const span = d.createElement('span');
+    const span = prepareElement({
+      tag: 'span',
+      children: [
+        {
+          tag: 'input',
+          id: 'cbKeepLogin',
+          type: 'checkbox',
+          checked: isKeepLogin,
+          events: {
+            change: (event) => {
+              isKeepLogin = event.target.checked;
+              setCookieBool(cookieKeepLogin, isKeepLogin);
+              if (isKeepLogin) {
+                timerCheckLogin = setupReloadTimer();
+              } else {
+                clearTimeout(timerCheckLogin);
+              }
+            },
+          },
+        },
+        {
+          tag: 'label',
+          htmlFor: 'cbKeepLogin',
+          textContent: 'Keep Login',
+        },
+      ],
+    });
     aAccount.parentNode.appendChild(span);
-    const checkbox = d.createElement('input');
-    checkbox.id = 'cbKeepLogin';
-    checkbox.type = 'checkbox';
-    checkbox.checked = isKeepLogin;
-    checkbox.addEventListener(
-      'change',
-      (event) => {
-        isKeepLogin = event.target.checked;
-        setCookieBool(cookieKeepLogin, isKeepLogin);
-        if (isKeepLogin) {
-          timerCheckLogin = setupReloadTimer();
-        } else {
-          clearTimeout(timerCheckLogin);
-        }
-      }
-    );
-    span.appendChild(checkbox);
-    const label = d.createElement('label');
-    label.htmlFor = 'cbKeepLogin';
-    label.textContent = 'Keep Login';
-    span.appendChild(label);
   }
 }
 
 function relief() {
+  w.addEventListener(
+    'message',
+    (event) => {
+      if (event.origin != location.origin || event.data.sender != keySender) { return; }
+      const iframe = getNodesByXpath(`//iframe[@name="${event.data.bossId}"]`)[0];
+      if (!iframe || !iframe.parentNode) { return; }
+      iframe.parentNode.removeChild(iframe);
+    }
+  );
   getNodesByXpath('//a[contains(@href,"/explore/coop/")]')
     .forEach((a) => {
-      const bossId = a.href.match(/\/explore\/coop\/(\d+)/)[1];
-      a.target = `boss${bossId}`;
-      a.click();
+      const iframe = prepareElement({
+        tag: 'iframe',
+        name: makeBossId(a.href),
+        src: a.href,
+      });
+      d.body.appendChild(iframe);
     });
   setupReloadTimer();
 }
@@ -135,7 +155,22 @@ function explore() {
     buttonAttack.click();
     return;
   }
-  setTimeout(() => { window.close(); }, 10 * 1000);
+  setTimeout(
+    () => {
+      (w.opener || w.parent).postMessage(
+        { sender: keySender, bossId: makeBossId(location.href) },
+        location.origin
+      );
+    },
+    10 * 1000
+  );
+}
+
+function makeBossId(uri) {
+  const bossId = uri.match(/\/explore\/coop\/(\d+)/)[1];
+  return !bossId
+    ? null
+    : `boss${bossId}`;
 }
 
 function getCookie(key) {
